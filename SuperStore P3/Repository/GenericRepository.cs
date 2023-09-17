f9889d7ce3211554a07e0e3fef1898af593c7616
SamaritanAI <dithipekoketso@gmail.com>
2023-09-17 22:09:04 +0200

    Suggested interface implemetation

--- SuperStore P3/Repository/GenericRepository.cs
+++ SuperStore P3/Repository/GenericRepository.cs
@@ -1,6 +1,42 @@
-﻿namespace EcoPower_Logistics.Repository
+﻿using System.Linq.Expressions;
+
+namespace EcoPower_Logistics.Repository
 {
-    public class GenericRepository
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
     {
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> expression)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
