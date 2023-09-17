ed2955b46bc973ed8bb396e99ba71e2bb6e4bae0
SamaritanAI <dithipekoketso@gmail.com>
2023-09-17 22:05:58 +0200

    Created Interface and Generic class

---
+++ SuperStore P3/Repository/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace EcoPower_Logistics.Repository
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        T Get(int id);
+        IEnumerable<T> GetAll();
+        IEnumerable<T> Find(Expression<Func<T, bool>> expression);
+        void Add(T entity);
+        void Delete(int id);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+
+    }
+}
