c1a38220cfd2a3db83bcfd25873b348dadd2b318
SamaritanAI <dithipekoketso@gmail.com>
2023-09-19 18:16:15 +0200

    created services model

---
+++ SuperStore P3/Models/Services.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace EcoPower_Logistics.Models
+{
+    public partial class Services
+    {
+        public Guid ServiceID { get; set;}
+        public string ServiceName { get; set; }
+        public string ServiceDescription { get; set; }
+
+        private DateTime createdDate;
+
+        public DateTime GetCreatedDate()
+        {
+            return createdDate;
+        }
+
+        public void SetCreatedDate(DateTime value)
+        {
+            createdDate = value;
+        }
+    }
+}
